752d0e684aa41ef20f85b3b9f8888339a9bbb87d
Jacob Harris <jharris@nytimes.com>
2008-11-28 12:39:47 -0500

    Added reviews method to Critic. Beefed up rdoc a little

--- lib/nytimes/movies/critic.rb
+++ lib/nytimes/movies/critic.rb
@@ -9,6 +9,12 @@ module Nytimes
 				end
 			end
 			
+			##
+			# Returns reviews made by the critic. Please refer to Review#find for other optional flags that can be applied.
+			def reviews(params={})
+				Review.find(params.merge :reviewer => seo_name)
+			end
+			
 			def self.create_from_api(params={})
 				self.new :display_name => params['display_name'],
 								 :sort_name => params['sort_name'],
@@ -18,6 +24,11 @@ module Nytimes
 								 :photo => MultimediaLink.create_from_api(params['multimedia'])
 			end
 			
+			##
+			# Returns a list of critics that match a given type. The following types are supported:
+			# * <tt>:full_time</tt> - fulltime critics at the New York Times
+			# * <tt>:part_time</tt> - part-time critics at the New York Times
+			# * <tt>:all</tt> - all critics
 			def self.find_by_type(type)
 				key = case type
 				when :full_time
@@ -35,11 +46,17 @@ module Nytimes
 				results.map {|r| self.create_from_api(r)}
 			end
 			
+			##
+			# Escapes a critic's name to the SEO variation used for name searches. This is automatically used by Critic#find_by_name, so you don't need to call it.
 			def self.escape_critic_name(name)
 				return name if name =~ /^[a-z\-]+$/  # might be escaped already
 				name.downcase.gsub(/[^a-z\s]/, ' ').gsub(/\s+/, '-')
 			end
 			
+			##
+			# Find a critic record matching a given name. Both the English name (eg, 'A. O. Scott') and the SEO name ('a-o-scott') are valid keys, although the SEO name is
+			# suggested if you want to avoid ambiguity. Only exact matches are used (no last name searches). If a single matching record is found, returns a single Critic instance.
+			# In the admittedly rare chance of 2 distinct critics having the same name, returns an array. Returns nil if no matches are found. 
 			def self.find_by_name(name)
 				name = escape_critic_name(name)
 				reply = invoke("critics/#{name}")
